668bc61dd2b85dd250b5e8eede542ff690188aa9
Asier Aguado <asier.aguado@protonmail.ch>
2018-07-14 13:32:32 +0200

    Use improved fall animations from cordova version

--- js/makefall.js
+++ js/makefall.js
@@ -17,15 +17,30 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 function getYPosition(element) {
         return $(element).position()['top'] - $(element).parent().position()['top'];
 }
+
+function getXPosition(element) {
+        return $(element).position()['left'] - $(element).parent().position()['left'];
+}
+
 function getFallDistance(element) {
         return ($(element).parent().outerHeight() - getYPosition(element)) - $(element).outerHeight();
 }
 function makeFall(element){
         $(element).removeAttr("id");
         var time = 1250;
+        var floordistance = getFallDistance(element);
+        var objects = 0;
         marginLeft = parseInt((count%2 !== 0 ? -0.45 : 0.32) * $(document).width() * Math.random());
+        do {
+		    obs = document.elementFromPoint( getXPosition(element) + marginLeft + $(element).width()/2, $(element).parent().outerHeight() - objects - $(element).height()/2 );
+	  		if (obs.nodeName == "IMG" && !$(obs).is(element)) {
+	  			objects += $(obs).height();
+	  		}
+	  	}
+	  	while (obs.nodeName == "IMG" && !$(obs).is(element));
+	  	if (objects > 0) objects -= $(element).height()/4;
         $(element).stop().animate({
-                marginTop: getFallDistance($(element)) + "px",
+                marginTop: (floordistance - objects) + "px",
                 marginLeft: marginLeft + "px"
         }, {
                 duration: time,
@@ -36,8 +51,18 @@ function makeFall(element){
 function simpleFall(element){
         $(element).removeAttr("id");
         var time = 1250;
+        var floordistance = getFallDistance(element);
+        var objects = 0;
+        do {
+		    obs = document.elementFromPoint( getXPosition(element) + $(element).width()/2, $(element).parent().outerHeight() - objects - $(element).height()/2 );
+	  		if (obs.nodeName == "IMG" && !$(obs).is(element)) {
+	  			objects += $(obs).height();
+	  		}
+	  	}
+	  	while (obs.nodeName == "IMG" && !$(obs).is(element));
+	  	if (objects > 0) objects -= $(element).height()/4;
         $(element).stop().animate({
-                marginTop: getFallDistance($(element)) + "px"
+                marginTop: (floordistance - objects) + "px"
         }, {
                 duration: time,
                 easing: "easeOutBounce"
